cc0c16ab7e24f93c9edb9651748455519b4339e2
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-01-17 01:21:46 +0100

    Add project files.

---
+++ KursyWalutzNBP/ReaderNBP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace KursyWalutzNBPClassLibrary
+{
+    public class ReaderNBP
+    {
+        private enum currency {USD, EUR, CHF, GBP}
+        private static string path = "http://www.nbp.pl/kursy/xml/";
+
+
+        public void read(DateTime firstDate, DateTime lastDate)
+        {
+            WebClient client = new WebClient();
+            string stringFileNames = "";
+            for(int year = firstDate.Year; year<= lastDate.Year; year++)
+            {
+                Stream stream;
+                try
+                {
+                    stream = client.OpenRead($"{path}dir{year}.txt");
+                }catch
+                {
+                    continue;
+                }
+                StreamReader reader = new StreamReader(stream);
+                stringFileNames += "\r\n"+reader.ReadToEnd();
+            }
+            var f = stringFileNames.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)
+                .Where(filename => filename != "" && filename[0] == 'c');
+
+        }
+
+        private string parseToString(DateTime date)
+        {
+            return string.Format("{0:yyMMdd}", date);
+        }
+
+
+    }
+}
---
+++ NBPConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KursyWalutzNBPClassLibrary;
+
+namespace NBPConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReaderNBP reader = new ReaderNBP();
+            reader.read(new DateTime(1999, 1, 1), DateTime.Now);
+        }
+    }
+}
